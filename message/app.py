24e09649fdd0a5da9bbe5b2dc52f35df3855267d
Maike2961 <mayki.silva@hotmail.com>
2023-10-25 15:01:14 -0300

    variavel de ambiente

--- message/app.py
+++ message/app.py
@@ -1,12 +1,17 @@
 import pandas as pd
 from twilio.rest import Client
-from teste import sid, token, from_tel
+#from teste import sid, token, from_tel
+from dotenv import load_dotenv
+import os
 
-sid1 = sid
-token1 = token
+load_dotenv(override=True)
+sid1 = os.getenv("sid")
+token1 = os.getenv("token")
+from_tel = os.getenv("from_tel")
 
 client = Client(sid1 , token1)
-
+print(sid1)
+print(token1)
 
 lista_meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho']
 
@@ -19,3 +24,4 @@ for mes in lista_meses:
       vendas = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendas'].values[0]
       print(f"No mês {mes}, O vendedor: {vendedor}, vendeu o total de: {vendas}, batendo a meta")
       message = client.messages.create(to=from_tel,from_="+15855427360", body= f"No mês {mes}, O vendedor: {vendedor}, vendeu o total de: {vendas}, batendo a meta")
+     
